fae538a32169df0931aca3150540b7a0cd69b890
polina.novokreschenova <noverianhenon@yandex.ru>
2020-06-07 15:55:20 +0300

    ignore warning

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'regenerator-runtime/runtime';
 import '../assets/application.scss';
 
 // import faker from 'faker';
+// @ts-ignore
 import gon from 'gon';
 // import cookies from 'js-cookie';
 // import io from 'socket.io-client';
