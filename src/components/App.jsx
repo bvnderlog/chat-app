ce6e95decd4c2ee4a58ae9a1570f01a885e93457
polina.novokreschenova <noverianhenon@yandex.ru>
2020-06-22 01:25:34 +0300

    update messages state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Channels from './Channels.jsx';
 import Chat from './Chat.jsx';
+import routes from '../routes.js';
+import axios from 'axios';
 
 
 export default class App extends React.Component {
@@ -10,6 +12,7 @@ export default class App extends React.Component {
             channels: props.channels,
             currentChannelId: props.currentChannelId,
             messages: props.messages,
+            inputText: '',
         };
     }
 
@@ -17,9 +20,40 @@ export default class App extends React.Component {
         this.setState({ currentChannelId: channelId });
     }
 
+    onFormChange = ({ target }) => {
+        this.setState({ inputText: target.value });
+    }
+
+    onFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        const { currentChannelId } = this.state;
+        const { channelMessagesPath } = routes;
+
+        const url = channelMessagesPath(currentChannelId);
+        const data = {
+            attributes: {
+                channelId: currentChannelId,
+                username: 'badcookie',
+                content: this.state.inputText,
+            }
+        }
+
+        const response = await axios.post(url, { data });   
+        const newMessage = response.data.data.attributes;
+        this.setState({ messages: [...this.state.messages, newMessage] });
+        this.setState({ inputText: '' });
+    }
+
     render() {
-        const { channels, currentChannelId, messages } = this.state;
+        const {
+            channels,
+            currentChannelId,
+            messages,
+            inputText,
+        } = this.state;
         const activeMessages = messages.filter(({ channelId }) => channelId === currentChannelId);
+
         return (
             <div className="h-100" id="chat">
                 <div className="row h-100 pb-3">
@@ -28,7 +62,13 @@ export default class App extends React.Component {
                         currentChannelId={currentChannelId}
                         onChannelClick={this.onChannelClick}
                     />
-                    <Chat messages={activeMessages} />
+                    <Chat
+                        messages={activeMessages}
+                        currentChannelId={currentChannelId}
+                        inputText={inputText}
+                        onFormChange={this.onFormChange}
+                        onFormSubmit={this.onFormSubmit}
+                    />
                 </div>
             </div>
         );
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import Form from './Form.jsx';
 
 
-const Chat = (props) => {
-    const { messages, currentChannelId } = props;
-    return (
-        <div className="col h-100">
-            <div className="d-flex flex-column h-100">
-                <div id="messages-box" className="chat-messages overflow-auto mb-3">
-                    {
-                        messages.map((message) => {
-                            const { username, content } = message;
-                            return <div><b>{username}</b>: {content}</div>;
-                        })
-                    }
-                </div>
-                <Form currentChannelId={currentChannelId} />
+export default class Chat extends React.Component {
+    renderForm() {
+        const { inputText, onFormSubmit, onFormChange } = this.props;
+        return (
+            <div className="mt-auto">
+                <form onSubmit={onFormSubmit} noValidate="" className="">
+                    <div className="form-group">
+                        <div className="input-group">
+                            <input
+                                onChange={onFormChange}
+                                name="body"
+                                className="form-control"
+                                value={inputText}
+                            />
+                            <div className="d-block invalid-feedback">&nbsp;</div>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </div>
-    );
-};
+        );
+    }
 
-export default Chat;
+    render() {
+        const { messages } = this.props;
+        return (
+            <div className="col h-100">
+                <div className="d-flex flex-column h-100">
+                    <div id="messages-box" className="chat-messages overflow-auto mb-3">
+                        {
+                            messages.map((message) => {
+                                const { username, content, id } = message;
+                                return <div key={id}><b>{username}</b>: {content}</div>;
+                            })
+                        }
+                    </div>
+                    {this.renderForm()}
+                </div>
+            </div>
+        );
+    }
+}
