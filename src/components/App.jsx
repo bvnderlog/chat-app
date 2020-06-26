14c79a946f887b4f34694d650aac8bd1aa6b9ba9
polina.novokreschenova <noverianhenon@yandex.ru>
2020-06-27 01:36:30 +0300

    refactor app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 
 import Chat from './Chat.jsx';
 import Channels from './Channels.jsx';
-import UserContext from './context.js';
 
 
-export default class App extends React.Component {
-    static contextType = UserContext;
+const App = () => (
+    <div className="h-100" id="chat">
+        <div className="row h-100 pb-3">
+            <Channels />
+            <Chat />
+        </div>
+    </div>
+);
 
-    render() {
-        return (
-            <div className="h-100" id="chat">
-                <div className="row h-100 pb-3">
-                    <Channels />
-                    <Chat />
-                </div>
-            </div>
-        );
-    }
-}
+export default App;
