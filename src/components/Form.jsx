e8363a63f27da3aa503d6e452e034a824275159a
polina.novokreschenova <noverianhenon@yandex.ru>
2020-06-28 16:25:31 +0300

    add form error handling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import { Formik } from 'formik';
+import cn from 'classnames';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Formik, Form as FormikForm, Field } from 'formik';
 
 import routes from '../routes';
 import UserContext from '../context';
@@ -16,6 +17,13 @@ const mapStateToProps = (state) => {
 @connect(mapStateToProps)
 class Form extends React.Component {
     static contextType = UserContext;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasNetworkError: false,
+        };
+    }
 
     handleSubmit = async (values, { setSubmitting, resetForm }) => {
         setSubmitting(false);
@@ -33,31 +41,47 @@ class Form extends React.Component {
             },
         };
 
-        await axios.post(url, { data });
+        const response = await axios.post(url, { data });
+        if (response.status !== 200) {
+            this.setState({ hasNetworkError: true });
+        }
     }
+
+    validate = () => (this.state.hasNetworkError ? { body: 'Network error' } : {});
 
     render() {
         return (
             <Formik
                 initialValues={{ body: '' }}
                 onSubmit={this.handleSubmit}
+                validateOnChange={false}
+                validateOnBlur={false}
+                validate={this.validate}
             >
-                {(formik) => (
-                    <div className="mt-auto">
-                        <form onSubmit={formik.handleSubmit} noValidate="" className="">
-                            <div className="form-group">
-                                <div className="input-group">
-                                    <input
-                                        id="body"
-                                        { ...formik.getFieldProps('body') }
-                                        className="form-control"
-                                    />
-                                    <div className="d-block invalid-feedback">&nbsp;</div>
-                                </div>
+                {
+                    ({ errors, touched }) => {
+                        const errorExists = errors.body && touched.body;
+                        const inputClasses = cn({
+                            'form-control': true,
+                            'is-invalid': errorExists,
+                        });
+                        return (
+                            <div className="mt-auto">
+                                <FormikForm>
+                                    <div className="form-group">
+                                        <div className="input-group">
+                                            <Field name="body" className={inputClasses} />
+                                            <div className="d-block invalid-feedback">
+                                                {errorExists && errors.body}
+                                                &nbsp;
+                                            </div>
+                                        </div>
+                                    </div>
+                                </FormikForm>
                             </div>
-                        </form>
-                    </div>
-                )}
+                        );
+                    }
+                }
             </Formik>
         );
     }
